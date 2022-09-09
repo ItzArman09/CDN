f2b4920e404c65d5b58eacbea013a29449b6b277
∆rman OP! <100463136+ItzArman09@users.noreply.github.com>
2022-09-09 04:40:30 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <style>
+body {
+  background-image: url('me/avatar.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+}
+</style>
+
+<h1 style="color: 00ffb3; ">Hey, This Page Dosent look good to you :(</h1>
+
+<?php echo "Only Owners can access this Page."; ?>
+
+</body>
+</html> 
